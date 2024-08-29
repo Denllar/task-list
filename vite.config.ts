a227fa3b8a5e79053beb36020858df557278a9b4
Denis Gabbasov <denisgabbasov06.09.2004@gmail.com>
2024-08-29 12:55:48 +0500

    add base in vite.config.ts 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/infoteks-test-task/",
+  base: command === "build" ? "/infoteks-test-task/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+)
