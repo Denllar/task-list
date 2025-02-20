081b6a5e2156774f64d4d3abe47932226ff573ef
Denis <denisgabbasov06.09.2004@gmail.com>
2025-02-20 22:27:25 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/infoteks-test-task/" : "/",
+  base: command === "build" ? "/task-list/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
