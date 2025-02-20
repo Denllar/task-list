05d4a56b7803789fdb7b96ebd8dcfbe03b2c1f54
Denis <denisgabbasov06.09.2004@gmail.com>
2025-02-20 22:45:12 +0500

    fix4

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: command === "build" ? "/task-list/" : "/",
+  // base: command === "build" ? "/task-list/" : "/",
+  base: "/task-list",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
